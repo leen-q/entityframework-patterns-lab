917b59746911da4f3df8320ff6eee4d40172f1ac
leen <eugeneverbitskiy84@gmail.com>
2021-11-23 17:53:46 +0200

    Added actors

---
+++ Solution1/CCL/Security/Identity/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security.Identity
+{
+    public class Worker : User
+    {
+        public Worker(int userId, string userLogin, string userPassword) : base(userId, userLogin, userPassword, nameof(Worker))
+        {
+        }
+    }
+}
